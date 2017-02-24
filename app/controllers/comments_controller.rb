00c2615ea5293a1e2a7d939331ba11fadefd696b
ohtasoji <ohtasoji0715@gmail.com>
2017-02-24 13:11:23 +0900

    コメント投稿

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -70,6 +70,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:sentence)
+      params.require(:comment).permit(:sentence, :memo_id)
     end
 end
